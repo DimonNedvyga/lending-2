74683d11de07bd9934c850664aa36ba7cc3b4417
DimonNedvyga <43214113+DimonNedvyga@users.noreply.github.com>
2018-10-22 20:26:12 +0300

    Update and rename smart.php to smart.example.php

--- mailer/smart.example.php
+++ mailer/smart.example.php
@@ -14,8 +14,8 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'nevigadimon@gmail.com';                 // Наш логин
-$mail->Password = 'asnaebrespect';                           // Наш пароль от ящика
+$mail->Username = 'EMAIL';                 // Наш логин
+$mail->Password = 'PASSWORD';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
